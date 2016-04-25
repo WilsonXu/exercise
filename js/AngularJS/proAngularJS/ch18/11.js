4cbeadf7eb2b0f8d4f5386ebc223b911e3946fe9
Wilson Xu <xuweipeng_ok@126.com>
2016-04-26 02:54:58 +0800

    complete ch15 for proangularjs

---
+++ js/AngularJS/proAngularJS/ch18/11.js
@@ -0,0 +1,13 @@
+angular.module("customServices", [])
+.provider("logService", function() {
+    return {
+        $get: function() {
+            return {
+                messageCount: 0,
+                log: function(msg) {
+                    console.log("Debug: " + (this.messageCount++) + " " + msg);
+                }
+            };
+        }
+    };
+});
