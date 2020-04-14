468c75272a091ded7649415d3d2bc58ffff6a022
Wilson Xu <xuweipeng_ok@126.com>
2020-04-15 02:18:23 +0800

    fix issues

--- js/nodejsinaction/ch01-welcome-to-node/hello.js
+++ js/nodejsinaction/ch01-welcome-to-node/hello.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const port = 8080;
 
-const  server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
     res.end("Hello, world.");
 });
 
